7685372de546fb2e06d93f8bd7131aa69550928c
aminmg <a.moghaddasi@gmail.com>
2014-03-10 15:20:14 +0100

    commit and push test

--- bcbio/ngsalign/test.py
+++ bcbio/ngsalign/test.py
@@ -5,7 +5,8 @@ sys.path.insert(0, '/home/moghadda/git/bcbio-nextgen/bcbio/pipeline')
 from config_utils import *
 
 
-config = load_config('/home/moghadda/git/bcbio-nextgen/config/examples/NA12878-ensemble.yaml')
+config = load_config('/home/moghadda/git/bcbio-nextgen/config/examples/NA12878-ensemble.yaml')    
+
 print config
 # config_utils.load_config('/home/moghadda/git/bcbio-nextgen/config/examples/NA12878-ensemble.yaml')
 # print cfg.load_config('/home/moghadda/git/bcbio-nextgen/config/examples/NA12878-ensemble.yaml')
